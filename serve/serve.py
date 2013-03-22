1e0da17bc8b55771d499d31e70eb9d8ab2a9181b
Matt Ball <yosemitebandit@gmail.com>
2013-03-22 10:31:10 -0700

    adds some more structure to server skeleton

--- serve/serve.py
+++ serve/serve.py
@@ -1,17 +1,33 @@
 ''' serve.py
 simple flask server
 '''
-from flask import Flask, request, abort, render_template
+from flask import Flask, request, abort, render_template, redirect, url_for
 
 app = Flask(__name__)
 app.config.from_envvar('REDREAM_SETTINGS')
 
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
-    ''' show the homepage
+    ''' GET to view the dream-input controls
+    POST to create a new dream-tage
     '''
-    return 'hullo!'
+    if request.method == 'GET':
+        return render_template('home.html')
+
+    if request.method == 'POST':
+        # make a new dream instance in the db
+        # initiate the workers for processing the description
+        # redirect to the invidiual dream page
+        return redirect(url_for('dream', dream_slug = dream.slug))
+
+
+@app.route('/<dream_slug>')
+def dream(dream_slug):
+    ''' GET to find a certain dream-tage
+    '''
+    # pull the dream from the db
+    return render_template('dream.html', dream=dream)
 
 
 if __name__ == '__main__':
