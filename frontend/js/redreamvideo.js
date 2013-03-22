443ebcda7cbd751f1bdeaebe6212af06b2e8468e
Takashi Mizohata <beatak@gmail.com>
2013-03-22 16:03:27 -0700

    basic funtionality for video object

---
+++ frontend/js/redreamvideo.js
@@ -0,0 +1,174 @@
+(function () {
+
+var RedreamVideo = window.RedreamVideo = function (selector, url) {
+  var $dom = $(selector);
+  var popcorn = Popcorn.smart(selector, url);
+  var video = popcorn.video;
+
+  popcorn.on(
+    'canplay', 
+    function () {
+      var ww = $dom.width();
+      var wh = $dom.height();
+      var w_ratio = ww / wh;
+      var vw = video.videoWidth;
+      var vh = video.videoHeight;
+      var v_ratio = vw / vh;
+      if (w_ratio === 1) {
+        // square
+        console.log('implement me!');
+      }
+      else if (w_ratio > 1) {
+        // landscape
+        if (v_ratio === 1) {
+          console.log( 'a0' );
+          // video is square
+          // video width align to wrap width
+          // video marginTop is always negative
+          video.style.width = ww + 'px';
+          video.style.marginTop = ((ww - (vw * wh / ww)) / 2) + 'px';
+        }
+        else if (v_ratio > 1) {
+          // video is landscape too
+          if (v_ratio === w_ratio) {
+            console.log( 'a1' );
+            // same aspect ratio
+            video.style.width = ww + 'px';
+          }
+          else if (v_ratio > w_ratio) {
+            console.log( 'a2' );
+            // video is wider than wrap
+            // video height is align to wrap height
+            // video marginLeft is always negative
+            video.style.height = wh + 'px';
+            video.style.marginLeft = ((ww - (vh * ww / wh)) / 2) + 'px';
+          }
+          else {
+            console.log( 'a3' );
+            // video is shorter than wrap
+            // video width align to wrap width
+            // video marginTop is always negative
+            video.style.height = wh + 'px';
+            video.style.marginLeft = ((ww - (vh * ww / wh)) / 2) + 'px';
+          }
+        }
+        else {
+          console.log( 'a4' );
+          // video is portrait
+          // video is shorter than wrap
+          // video width align to wrap width
+          // video marginTop is always negative
+          video.style.height = wh + 'px';
+          video.style.marginLeft = ((ww - (vh * ww / wh)) / 2) + 'px';
+        }
+      }
+      else {
+        // portrait
+        console.log('implement me!');
+      }
+    }
+  );
+
+  console.log( 'hey!!', $dom );
+  this.dom = $dom[0];
+
+  console.log( this.dom );
+
+  $dom.addClass( 'popcorn-wrap' );
+  video.removeAttribute('controls');
+  video.muted = true;
+  popcorn.loop(true);
+  
+  RedreamVideo._global.push(this);
+  this.popcorn = popcorn;
+};
+
+RedreamVideo._global = [];
+
+RedreamVideo.globalHalt = function () {
+  $.each(
+    RedreamVideo._global, 
+    function (i, rv) {
+      if (!rv.popcorn.isDestroyed) {
+        rv.popcorn.pause();
+      }
+    }
+  );
+};
+
+RedreamVideo.globalPlay = function () {
+  $.each(
+    RedreamVideo._global, 
+    function (i, rv) {
+      if (!rv.popcorn.isDestroyed) {
+        rv.popcorn.play();
+      }
+    }
+  );
+};
+
+RedreamVideo.globalMute = function () {
+  $.each(
+    RedreamVideo._global, 
+    function (i, rv) {
+      if (!rv.popcorn.isDestroyed) {
+        rv.popcorn.video.muted = true;
+      }
+    }
+  );
+};
+
+RedreamVideo.globalUnmute = function () {
+  $.each(
+    RedreamVideo._global, 
+    function (i, rv) {
+      if (!rv.popcorn.isDestroyed) {
+        rv.popcorn.video.muted = false;
+      }
+    }
+  );
+};
+
+RedreamVideo.prototype.play = function () {
+  if (this.popcorn && !this.popcorn.isDestroyed) {
+    this.popcorn.play();
+  }
+  return this;
+};
+
+RedreamVideo.prototype.pause = function () {
+  if (this.popcorn && !this.popcorn.isDestroyed) {
+    this.popcorn.pause();
+  }
+  return this;
+};
+
+RedreamVideo.prototype.cue = function (time, cb) {
+  if (this.popcorn && !this.popcorn.isDestroyed) {
+    this.popcorn.cue(time, cb);
+  }
+  return this;
+};
+
+RedreamVideo.prototype.bringFront = function () {
+  $.each( 
+    RedreamVideo._global,
+    function (i, rv) {
+      $(rv.dom).removeClass('is-above');
+    }
+  );
+  $(this.dom).addClass('is-above');
+  return this;
+};
+
+RedreamVideo.prototype.fade = function () {
+  $(this.dom).addClass('is-hiding');
+  return this;
+};
+
+RedreamVideo.prototype.unfade = function () {
+  $(this.dom).removeClass('is-hiding');
+  return this;
+};
+
+})();
---
+++ frontend/js/testbed.js
@@ -0,0 +1,61 @@
+/**
+ * test test test
+ */
+(function() {
+
+var video_hash = {
+  '20526565': 'http://av.vimeo.com/71427/795/41518371.mp4?aksessionid=25a238d012dcb3822562ee3a4151e6b4&token=1363996617_22f9ff633c526e9c3dee3c938739c338'
+  , '32958521': 'http://av.vimeo.com/46287/006/74867996.mp4?aksessionid=312d134c2a88d2cb01e030588cd0e245&token=1363997165_4423d8225f80cbbba621a016c8629cc2'
+  , '18280328' : 'http://av.vimeo.com/43382/164/36199250.mp4?aksessionid=1ddc1b47e04db75cd42d21aef00fede6&token=1363997231_b7575d9b4b377e2b33e14645cd277610'
+};
+
+var video_list = [];
+var loaded_video = [];
+for (var _key in video_hash) {
+  video_list.push( video_hash[ _key ] );
+}
+
+var $wrapper;
+var tmpl_video_wrap;
+var prefix_id = 'video_';
+var is_inited = false;
+var buffers = window.buffers = [];
+
+var init = function () {
+  console.log('init');
+
+  $wrapper = $('#wrapper');
+  tmpl_video_wrap = $.trim( $('#tmpl-video-wrap').html() );
+  startLoading();
+
+  setTimetout( switching, 10000 );
+};
+
+var startLoading = function () {
+  if (video_list.length !== loaded_video.length) {
+    console.log( 'load another video' );
+    var i = loaded_video.length;
+    var url = video_list[ i ];
+    var id = prefix_id + i;
+    $wrapper.append(Mustache.render(tmpl_video_wrap, {ID: id}));
+    var rv = new RedreamVideo( '#' + id, url );
+    loaded_video.push( url );
+    buffers.push( rv );
+
+    if (!is_inited) {
+      rv.play().bringFront();
+      is_inited = true;
+    }
+
+    setTimeout(startLoading, 1000);
+  }
+};
+
+var switching = function () {
+  
+};
+
+
+$(init);
+
+})();
